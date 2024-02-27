6c7ff0da8bbdf38441dca1f5f14dc06562182f8d
jodan <wdd789q@naver.com>
2024-02-27 15:15:16 +0900

    Expo 셋팅후 web 셋팅

--- App.js
+++ App.js
@@ -4,7 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Open up App.js to start working on your gdgdsgsadfasf!</Text>
+      <View style={styles.subcontainer}>
+        이것은 view 안의 view이다.
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +19,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    borderColor: 'black',
   },
+  text: {
+    color: 'red',
+  },
+  subcontainer: {
+    width: '100%',
+    border: '1px solid black',
+  }
 });
